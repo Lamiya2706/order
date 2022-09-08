17a6b040ced87d9ccfbcc2dc70e0d1be0812558d
Lamiya Asgarova <lamiyaasgarova@Mac-005559.local>
2022-09-08 11:13:48 +0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
+const path = require('path');
+const deps = require('./package.json').dependencies;
+
+module.exports = (_, argv) => ({
+  entry: './src/index',
+  // mode: 'development',
+  output: {
+    publicPath: argv.mode === 'development' ? 'localhost:3000' : 'https://lamiya2706.github.io/order'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    // port: 3002,
+    historyApiFallback: true,
+    hot: 'only',
+  },
+  output: {
+    publicPath: 'auto',
+    chunkFilename: '[id].[contenthash].js',
+  },
+  resolve: {
+    extensions: ['.js', '.mjs', '.jsx', '.css'],
+    alias: {
+      events: 'events',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'order',
+      filename: 'remoteEntry.js',
+      remotes: {
+        // shell: 'shell@http://localhost:3000/remoteEntry.js',
+        shell: 'shell@https://lamiya2706.github.io/shell/remoteEntry.js',
+      },
+      exposes: {
+        './OrderService': './src/OrderService',
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+        '@material-ui/core': {
+          singleton: true,
+          requiredVersion: deps['@material-ui/core'],
+        }
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      publicPath: './',
+    }),
+  ],
+});
